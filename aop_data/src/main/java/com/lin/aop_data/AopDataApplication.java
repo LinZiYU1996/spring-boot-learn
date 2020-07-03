fee72ac6dcc284adf09a8f6b251cd90dbb3fc3be
LinZiYu1996 <2669093302@qq.com>
2020-07-03 21:59:28 +0800

    7-3

---
+++ aop_data/src/main/java/com/lin/aop_data/AopDataApplication.java
@@ -0,0 +1,13 @@
+package com.lin.aop_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopDataApplication.class, args);
+    }
+
+}
